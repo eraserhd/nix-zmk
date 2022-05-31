27a5733fad2e11a0d54615976bb33a8ef9acff3e
Jason Felice <jason.m.felice@gmail.com>
2022-05-31 10:36:55 -0400

    Add defaultZmk

--- flake.nix
+++ flake.nix
@@ -1,5 +1,16 @@
 {
-  outputs = { self, nixpkgs }: {
-    lib = {};
-  };
+  outputs = { self, nixpkgs }:
+    let
+      defaultZmk = { buildSystem }: nixpkgs.legacyPackages.${buildSystem}.fetchFromGitHub {
+        owner = "zmkfirmware";
+        repo = "zmk";
+        rev = "41dc774848dace9b4bcfa59691c81a229dd416e1";
+        sha256 = "/BIVgqOfavHVIIVGLfg7rOx6T8GPhEjOsokvq1uw6sw=";
+      };
+
+    in {
+      lib = {
+        inherit defaultZmk;
+      };
+    };
 }
